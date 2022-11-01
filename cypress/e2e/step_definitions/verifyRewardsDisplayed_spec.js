b3023474b09e0a9b515c0d8827e333d8a0ae4965
Carlos <carlossik@gmail.com>
2022-11-01 12:51:39 +0000

    created new working branch, added some step defs

---
+++ cypress/e2e/step_definitions/verifyRewardsDisplayed_spec.js
@@ -0,0 +1,3 @@
+import { Given, Then, When } from '@badeball/cypress-cucumber-preprocessor';
+import homePage from '../../support/pageobjects/home_page';
+import signInPage from '../../support/pageobjects/signIn_page';
