a3ec6f92f0eae6e853c48b7d16ead919e7a92bd2
Carlos <carlossik@gmail.com>
2023-01-08 18:47:30 +0000

    modified step def for how to collect rewards

--- cypress/e2e/step_definitions/howTocollectRewards.js
+++ cypress/e2e/step_definitions/howTocollectRewards.js
@@ -6,12 +6,16 @@ const logUser = require('../loginScript').default;
 
 Given('A user is already Logged in', function () {
   logUser.login();
-  homePage.clickAcceptCookie();
+  //homePage.clickAcceptCookie();
 });
 
 Then('They can see how to collect rewards', function () {
-  return 'pending';
+    cy.get('.earning-activities__activity--featured')
+    .should('be.visible')
 });
+Then("They can see how to collect rewards slider", function () {
+    return "pending";
+    });
 Given('User slides the slider button', function () {
   return 'pending';
 });
