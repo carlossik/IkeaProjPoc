ed32d1eef4c9b0b92dadb053e3848b842a5a058c
Carlos <carlossik@gmail.com>
2023-01-25 10:23:12 +0000

    refactored env files with setenv.sh and refactoring login file to accomodate new changes

--- cypress.config.js
+++ cypress.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 const { defineConfig } = require('cypress');
 const createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
 const preprocessor = require('@badeball/cypress-cucumber-preprocessor');
@@ -66,5 +67,20 @@ module.exports = defineConfig({
       step_definitions: 'cypress/e2e/**/*.{cy,js,ts}',
       ///excludeSpecPattern: ['*.js'],
     },
-  },
+    env: {
+      "PT_USER_NAME": "sapt99@mailinator.com",
+      "PT_PASSWORD":`${process.env.CYPRESS_PT_PASSWORD}`,
+      "ITuserName":"sait1@mailinator.com",
+      "ITpassWord":"Sample123!",
+      "PTURL": "https://www.ikea.com/pt/en",
+      "PT_AUTH_URL":"https://pt.accounts.ikea.com/oauth/token",
+      "PT_CLIENT_ID": "5ylUcKnvwzRJTN7Ieguw26jtwfaUSQzR",
+      "PT_CLIENT_SECRET": "x55QDagwC_aW2MvNdlmEmoxxBMNJzY2nDE-xNGanbdHJJ_0-kL2gO_kR57ha9Wy9",
+      "ITauthUrl":"https://it.accounts.ikea.com/oauth/token",
+      "ITClientID": "WlOGA2kjqvo8ZvRoDQu1bQtoQV7ODyqu",
+      "ITClientSecret": "QEtSGLA6LFFTXvB6krkyA-GDNQj6Up7q81aYYuOt6h9GfMDp8hZtipNItMNl6sGE",
+      "ITURL": "https://www.ikea.com/it/en"
+     
+    }
+  }
 });
