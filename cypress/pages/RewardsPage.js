bf2ca73df2a867fa71b80b9bf1456f19bbf9486e
Carlos <carlossik@gmail.com>
2023-07-06 11:14:01 +0100

    made changes to reward id's, added script to run specific tests for regression

--- cypress/pages/RewardsPage.js
+++ cypress/pages/RewardsPage.js
@@ -2,38 +2,38 @@ class RewardsPage {
 
 
 ptRewards = {
-cakeAndCoffeeForOne : "[data-testid=\"reward-4989\"] > .rewards__rewards__inner",
-completeMealForOne : "[data-testid=\"reward-4907\"] > .rewards__rewards__inner",
-completeMealForTwo: "[data-testid=\"reward-4901\"] > .rewards__rewards__inner",
-voucherForParcelDelivery : "[data-testid=\"reward-4903\"] > .rewards__rewards__inner",
-voucherForHomeDelivery:"[data-testid=\"reward-4900\"] > .rewards__rewards__inner",
-ikeaProductVoucher10: "[data-testid=\"reward-4908\"] > .rewards__rewards__inner",
-voucherForClickCollectPickup:"[data-testid=\"reward-4904\"] > .rewards__rewards__inner",
-voucherForSideWalkDelivery: "[data-testid=\"reward-4988\"] > .rewards__rewards__inner",
-ikeaProductVoucher15:"[data-testid=\"reward-4902\"] > .rewards__rewards__inner"
+cakeAndHotDrinkForOne : '[data-testid="reward-PT-PRIVATE-LOYALTY-KEYS-OFFER-16"] > .rewards__rewards__inner',
+completeMealForOne : '[data-testid="reward-PT-PRIVATE-LOYALTY-KEYS-OFFER-10"] > .rewards__rewards__inner',
+completeMealForTwo: '[data-testid="reward-PT-PRIVATE-LOYALTY-KEYS-OFFER-11"] > .rewards__rewards__inner',
+voucherForParcelDelivery : '[data-testid="reward-PT-PRIVATE-LOYALTY-KEYS-OFFER-1"] > .rewards__rewards__inner',
+voucherForHomeDelivery:'[data-testid="reward-PT-PRIVATE-LOYALTY-KEYS-OFFER-5"] > .rewards__rewards__inner',
+ikeaProductVoucher10: '[data-testid="reward-PT-PRIVATE-LOYALTY-KEYS-OFFER-14"] > .rewards__rewards__inner',
+voucherForClickCollectPickup:'[data-testid="reward-PT-PRIVATE-LOYALTY-KEYS-OFFER-3"] > .rewards__rewards__inner',
+cakeAndHotDrinkForTwo:'[data-testid="reward-PT-PRIVATE-LOYALTY-KEYS-OFFER-17"] > .rewards__rewards__inner',
+ikeaProductVoucher15:'[data-testid="reward-PT-PRIVATE-LOYALTY-KEYS-OFFER-15"] > .rewards__rewards__inner'
 
 }
 auRewards = {
-save5$onOnlineDelivery:"[data-testid=\"reward-5060\"] > .rewards__rewards__inner",
-freeDaimeCakeSliceandHotDrink:"[data-testid=\"reward-5063\"] > .rewards__rewards__inner",
-save$10onIkeaHomeFurnishings:"[data-testid=\"reward-5065\"] > .rewards__rewards__inner",
-freeEatmealatSwedRestaurant:"[data-testid=\"reward-5064\"] > .rewards__rewards__inner",
-save$15onOnlineDelivery:"[data-testid=\"reward-5061\"] > .rewards__rewards__inner",
-save$30OnOnlineDelivery:"[data-testid=\"reward-5062\"] > .rewards__rewards__inner",
-save$20OnIkeaHomeFurnishings:"[data-testid=\"reward-5062\"] > .rewards__rewards__inner"
+save5$onOnlineDelivery:'[data-testid="reward-AU-PRIVATE-LOYALTY-KEYS-OFFER-1"] > .rewards__rewards__inner',
+freeDaimeCakeSliceandHotDrink:'[data-testid="reward-AU-PRIVATE-LOYALTY-KEYS-OFFER-4"] > .rewards__rewards__inner',
+save$10onIkeaHomeFurnishings:'[data-testid="reward-AU-PRIVATE-LOYALTY-KEYS-OFFER-4"] > .rewards__rewards__inner',
+freeEatmealatSwedRestaurant:'[data-testid="reward-AU-PRIVATE-LOYALTY-KEYS-OFFER-5"] > .rewards__rewards__inner',
+save$15onOnlineDelivery:'[data-testid="reward-AU-PRIVATE-LOYALTY-KEYS-OFFER-2"] > .rewards__rewards__inner',
+save$30OnOnlineDelivery:'[data-testid="reward-AU-PRIVATE-LOYALTY-KEYS-OFFER-3"] > .rewards__rewards__inner',
+save$20OnIkeaHomeFurnishings:'[data-testid="reward-AU-PRIVATE-LOYALTY-KEYS-OFFER-7"] > .rewards__rewards__inner'
 
 }
 
 itRewards ={
-vegetarianMenuFor1 : "[data-testid=\"reward-5141\"] > .rewards__rewards__inner",
-breakfastMenuAt1 : "[data-testid=\"reward-4911\"] > .rewards__rewards__inner",
-dessertMenufor1 : "[data-testid=\"reward-4916\"] > .rewards__rewards__inner",
-menuVegeTaliandWhichMeatballs: "[data-testid=\"reward-5143\"] > .rewards__rewards__inner",
-voucherOnfurnitureAndAccessories : "[data-testid=\"reward-4913\"] > .rewards__rewards__inner",
-voucherForHomeDelivery:"[data-testid=\"reward-5140\"] > .rewards__rewards__inner",
-voucherOnFurnitureAndAccessories: "[data-testid=\"reward-4918\"] > .rewards__rewards__inner",
-voucherOnDeliveryAndCollection:"[data-testid=\"reward-5139\"] > .rewards__rewards__inner",
-swedishMeatballMenufor1:"[data-testid=\"reward-5142\"] > .rewards__rewards__inner"
+vegetarianMenuFor1 : '[data-testid="reward-IT-PRIVATE-LOYALTY-KEYS-OFFER-4"] > .rewards__rewards__inner',
+breakfastMenuAt1 : '[data-testid="reward-IT-PRIVATE-LOYALTY-KEYS-OFFER-5"] > .rewards__rewards__inner',
+dessertMenufor1 : '[data-testid="reward-IT-PRIVATE-LOYALTY-KEYS-OFFER-6"] > .rewards__rewards__inner',
+menuVegeTaliandWhichMeatballs: '[data-testid="reward-IT-PRIVATE-LOYALTY-KEYS-OFFER-13"] > .rewards__rewards__inner',
+voucherOnfurnitureAndAccessories : '[data-testid="reward-IT-PRIVATE-LOYALTY-KEYS-OFFER-9"] > .rewards__rewards__inner',
+voucherForHomeDelivery:'[data-testid="reward-IT-PRIVATE-LOYALTY-KEYS-OFFER-12"] > .rewards__rewards__inner',
+voucherOnFurnitureAndAccessories: '[data-testid="reward-IT-PRIVATE-LOYALTY-KEYS-OFFER-10"] > .rewards__rewards__inner',
+voucherOnDeliveryAndCollection:"[data-testid=\"reward-IT-PRIVATE-LOYALTY-KEYS-OFFER-11\"] > .rewards__rewards__inner",
+swedishMeatballMenufor1:'[data-testid="reward-IT-PRIVATE-LOYALTY-KEYS-OFFER-14"] > .rewards__rewards__inner'
 
 
 }
